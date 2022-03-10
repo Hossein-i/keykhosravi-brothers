cb3fcf05f5d2424f31934f08e836a2c7c133aed3
Hossein <20hossein00@gmail.com>
2022-03-10 14:42:22 +0330

    hero-header end

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,36 @@ menuButton.addEventListener("click", function(){
     menuButton.classList.toggle("open")
     menuMobile.classList.toggle("show")
 })
+// slideshow
+let slides = document.getElementsByClassName("slideshow__slide"),
+    dots = document.querySelectorAll(".slideshow__pagination span"),
+    slideIndex = 1
+
+showSlides(slideIndex)
+autoSlides()
+
+const navSlides = (n) => showSlides(slideIndex += n)
+const currentSlides = (n) => showSlides(slideIndex = n)
+
+function showSlides(n) {
+    n > slides.length ? slideIndex = 1 : null
+    n < 1 ? slideIndex = slides.length : null
+
+    for (const slide of slides) slide.style.display = "none"
+    for (const dot of dots) dot.classList.remove("active")
+    
+    slides[slideIndex-1].style.display = "block"
+    dots[slideIndex-1].classList.add("active")
+}
+function autoSlides() {
+    for (const slide of slides) slide.style.display = "none"
+    for (const dot of dots) dot.classList.remove("active")
+
+    slideIndex++
+    slideIndex > slides.length ? slideIndex = 1 : null
+
+    slides[slideIndex-1].style.display = "block"    
+    dots[slideIndex-1].classList.add("active")
+
+    setTimeout(autoSlides, 5000)
+}
