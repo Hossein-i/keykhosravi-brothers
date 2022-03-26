b11f12cb7d757420fdb3b59df76255f265c97137
Hossein <20hossein00@gmail.com>
2022-03-26 12:16:15 +0430

    end services and fixed hero-header

--- scripts/main.js
+++ scripts/main.js
@@ -1,41 +1,46 @@
 // open and close menu mobile
 const menuButton = document.getElementById("menu-button"),
-      menuMobile = document.getElementById("menu-mobile")
+  menuMobile = document.getElementById("menu-mobile");
 
-menuButton.addEventListener("click", function(){
-    menuButton.classList.toggle("open")
-    menuMobile.classList.toggle("show")
-})
+menuButton.addEventListener("click", function () {
+  menuButton.classList.toggle("open");
+  menuMobile.classList.toggle("show");
+});
 // slideshow
 let slides = document.getElementsByClassName("slideshow__slide"),
-    dots = document.querySelectorAll(".slideshow__pagination span"),
-    slideIndex = 1
+  pagination = document.querySelector(".slideshow__pagination"),
+  slideIndex = 0;
 
-showSlides(slideIndex)
-autoSlides()
+for (let index = 0; index < slides.length; index++)
+  pagination.innerHTML += `<span onclick="currentSlides(${index + 1})"></span>`;
 
-const navSlides = (n) => showSlides(slideIndex += n)
-const currentSlides = (n) => showSlides(slideIndex = n)
+let dots = document.querySelectorAll(".slideshow__pagination span");
+
+showSlides(slideIndex);
+autoSlides();
+
+const navSlides = (n) => showSlides((slideIndex += n));
+const currentSlides = (n) => showSlides((slideIndex = n));
 
 function showSlides(n) {
-    n > slides.length ? slideIndex = 1 : null
-    n < 1 ? slideIndex = slides.length : null
+  n > slides.length ? (slideIndex = 1) : null;
+  n < 1 ? (slideIndex = slides.length) : null;
 
-    for (const slide of slides) slide.style.display = "none"
-    for (const dot of dots) dot.classList.remove("active")
-    
-    slides[slideIndex-1].style.display = "block"
-    dots[slideIndex-1].classList.add("active")
+  for (const slide of slides) slide.style.display = "none";
+  for (const dot of dots) dot.classList.remove("active");
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].classList.add("active");
 }
 function autoSlides() {
-    for (const slide of slides) slide.style.display = "none"
-    for (const dot of dots) dot.classList.remove("active")
+  for (const slide of slides) slide.style.display = "none";
+  for (const dot of dots) dot.classList.remove("active");
 
-    slideIndex++
-    slideIndex > slides.length ? slideIndex = 1 : null
+  slideIndex++;
+  slideIndex > slides.length ? (slideIndex = 1) : null;
 
-    slides[slideIndex-1].style.display = "block"    
-    dots[slideIndex-1].classList.add("active")
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].classList.add("active");
 
-    setTimeout(autoSlides, 5000)
+  setTimeout(autoSlides, 5000);
 }
